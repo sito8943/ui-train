d24d754f0dfabe1aa830e9687accb8b9aa731408
sito8943 <sito8943@gmail.com>
2022-04-01 11:50:11 -0400

    image card css

--- src/layouts/uiuxdesig.trends/Museums Virtually/components/Card/ImageCard/ImageCard.jsx
+++ src/layouts/uiuxdesig.trends/Museums Virtually/components/Card/ImageCard/ImageCard.jsx
@@ -12,31 +12,87 @@ import { BsBookmarkDash, BsFillStarFill } from "react-icons/bs";
 // components
 import ImageShimmer from "../../../../../../components/ImageShimmer/ImageShimmer";
 
+// style
+import "./style.css";
+
 const ImageCard = (props) => {
   const { title, noMarker, src } = props;
 
-  const imageCss = css({});
+  const titleArea = css({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "90%",
+    zIndex: 1,
+  });
 
-  const titleArea = css({});
+  const vrArea = css({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    background: "#9b9999",
+    padding: "0 10px",
+    borderRadius: 5,
+    zIndex: 1,
+    height: 42,
+  });
 
-  const vrArea = css({});
+  const vrButton = css({
+    color: "aliceblue",
+    fontWeight: 700,
+    background: "none",
+    border: "none",
+  });
 
-  const imageCardCss = css({});
+  const vrHr = css({
+    margin: "0 10px",
+    height: "90%",
+    borderColor: "#f0f8ff5e",
+  });
+
+  const starSpan = css({
+    color: "aliceblue",
+    display: "flex",
+  });
+
+  const vrStarIcon = css({
+    marginLeft: "7px",
+  });
+
+  const imageCardCss = css({
+    height: 285,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+  });
+
+  const markerButton = css({
+    border: "none",
+    color: "aliceblue",
+    padding: 10,
+    fontSize: 18,
+    background: "#bdbbbb",
+    cursor: "pointer",
+    height: 37,
+  });
 
   return (
-    <div className={imageCardCss}>
-      <ImageShimmer className={imageCss} src={src} />
+    <div className={`${imageCardCss} image-card`}>
+      <ImageShimmer width={285} height={285} src={src} />
       <div className={titleArea}>
         <h4>{title}</h4>
-        <button>
-          <BsBookmarkDash />
-        </button>
+        {!noMarker && (
+          <button className={markerButton}>
+            <BsBookmarkDash />
+          </button>
+        )}
       </div>
       <div className={vrArea}>
-        <button>Watch in VR</button>
-        <hr />
-        <span>
-          4.5 <BsFillStarFill />
+        <button className={vrButton}>Watch in VR</button>
+        <hr className={vrHr} />
+        <span className={starSpan}>
+          4.5 <BsFillStarFill className={vrStarIcon} />
         </span>
       </div>
     </div>

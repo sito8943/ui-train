00d6fe807aa79f419ca8d159fabce4db2f1c2a14
sito8943 <sito8943@gmail.com>
2022-04-01 10:44:11 -0400

    working with image card and image shimmer

--- src/layouts/uiuxdesig.trends/Museums Virtually/components/Card/ImageCard/ImageCard.jsx
+++ src/layouts/uiuxdesig.trends/Museums Virtually/components/Card/ImageCard/ImageCard.jsx
@@ -13,22 +13,44 @@ import { BsBookmarkDash, BsFillStarFill } from "react-icons/bs";
 import ImageShimmer from "../../../../../../components/ImageShimmer/ImageShimmer";
 
 const ImageCard = (props) => {
-  const { title, src } = props;
+  const { title, noMarker, src } = props;
+
+  const imageCss = css({});
+
+  const titleArea = css({});
+
+  const vrArea = css({});
+
+  const imageCardCss = css({});
+
   return (
-    <div>
-      <ImageShimmer src={src} />
-      <div>
+    <div className={imageCardCss}>
+      <ImageShimmer className={imageCss} src={src} />
+      <div className={titleArea}>
         <h4>{title}</h4>
         <button>
           <BsBookmarkDash />
         </button>
       </div>
+      <div className={vrArea}>
+        <button>Watch in VR</button>
+        <hr />
+        <span>
+          4.5 <BsFillStarFill />
+        </span>
+      </div>
     </div>
   );
 };
 
+ImageCard.defaultProps = {
+  noMarker: false,
+  title: "",
+};
+
 ImageCard.propTypes = {
-  title: PropTypes.string.isRequired,
+  noMarker: PropTypes.bool,
+  title: PropTypes.string,
   src: PropTypes.string.isRequired,
 };
 

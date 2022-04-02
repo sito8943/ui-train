59970caa994f29de07bcc4389335a67c094a717e
sito8943 <sito8943@gmail.com>
2022-04-02 15:51:09 -0400

    added accordion text

---
+++ src/layouts/uiuxdesig.trends/Museums Virtually/components/AccordionText/styles.jsx
@@ -0,0 +1,21 @@
+// emotion
+import { css } from "@emotion/css";
+
+const accordionCss = css({
+  width: "80%",
+  transition: "all 200ms ease",
+});
+
+const pCss = css({
+  marginTop: -5,
+  lineHeight: "30px",
+  fontSize: 15,
+  color: "#9f9d9d",
+});
+
+const readMoreSpan = css({
+  fontWeight: "600",
+  color: "#545252",
+});
+
+export { readMoreSpan, accordionCss, pCss };

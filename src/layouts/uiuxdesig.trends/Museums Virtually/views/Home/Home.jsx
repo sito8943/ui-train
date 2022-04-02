70aa1ccd6308647afc7cb12ff24be03eab7ec2bc
sito8943 <sito8943@gmail.com>
2022-04-02 09:48:12 -0400

    Mobile layout

--- src/layouts/uiuxdesig.trends/Museums Virtually/views/Home/Home.jsx
+++ src/layouts/uiuxdesig.trends/Museums Virtually/views/Home/Home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { css } from "@emotion/css";
-
 // components
 import Navbar from "../../components/Navbar/Navbar";
 import Search from "../../components/Search/Search";
@@ -9,6 +7,7 @@ import Dock from "../../components/Dock/Dock";
 import TabView from "../../layouts/TabView/TabView";
 import ImageCard from "../../components/Card/ImageCard/ImageCard";
 import Slideshow from "../../layouts/Slideshow/Slideshow";
+import Mobile from "../../layouts/Mobile/Mobile";
 
 // images
 import img1 from "../../../../../assets/images/img1.jpg";
@@ -17,7 +16,7 @@ import img3 from "../../../../../assets/images/img3.jpg";
 
 // styles
 import "./style.css";
-import { SeenProvider } from "../../context/SeenContext";
+import { span, titleArea, titleCss, spanTitle } from "./styles";
 
 const tabs = ["Popular", "For You", "India", "China", "Japan"];
 
@@ -33,47 +32,23 @@ const tabContent = [
 ];
 
 function Home() {
-  const containerCss = css({
-    width: 350,
-    height: 675,
-    padding: "10px 0",
-    background: "#f5f3f3",
-    boxShadow: "55px 20px 88px 15px #00000070",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    margin: "0 50px",
-  });
-
-  const titleArea = css({
-    width: "100%",
-    marginLeft: 50,
-  });
-
-  const titleCss = css({
-    width: "69%",
-  });
-
-  const spanTitle = css({
-    color: "#bdbbbb",
-  });
-
   return (
-    <div className={containerCss}>
-      <SeenProvider>
-        <Navbar />
-        <div className={titleArea}>
-          <h2 className={titleCss}>
-            Visit your most favorite{" "}
-            <span className={spanTitle}>Museums Virtually</span>
-          </h2>
-        </div>
+    <Mobile>
+      <Navbar />
+      <div className={titleArea}>
+        <h2 className={titleCss}>
+          Visit your most favorite{" "}
+          <span className={spanTitle}>Museums Virtually</span>
+        </h2>
+      </div>
 
-        <Search />
-        <TabView tabs={tabs} content={tabContent} />
-        <Dock />
-      </SeenProvider>
-    </div>
+      <Search />
+      <TabView tabs={tabs} content={tabContent} />
+      <span className={span} onClick={null}>
+        Show all
+      </span>
+      <Dock />
+    </Mobile>
   );
 }
 
